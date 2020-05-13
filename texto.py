1f2a31a0194f25f403b73a3f14867a50e6fb0e8e
Karen Vicente <karenyasmin37@gmail.com>
2020-05-13 17:30:41 -0300

    add files

---
+++ texto.py
@@ -0,0 +1,39 @@
+
+import mincemeat
+import glob
+import csv
+
+text_files = glob.glob('textos\\*')
+
+def file_contents(file_name):
+    f = open(file_name)
+    try:
+        return f.read()
+    finally:
+        f.close()
+
+source = dict((file_name, file_contents(file_name))for file_name in text_files)
+
+def mapfn(k, v):
+    print('map ' + k)
+    from stopwords import allStopWords
+    for line in v.splitlines():
+        for word in line.split():
+            if (word not in allStopWords):
+                yield word, 1
+                
+def reducefn(k, v):
+    print('reduce ' + k)
+    return sum(v)
+
+s = mincemeat.Server()
+
+# A fonte de dados pode ser qualquer objeto do tipo dicionário
+s.datasource = source
+s.mapfn = mapfn
+s.reducefn = reducefn
+
+results = s.run_server(password = "changeme")
+w = cvs.writer(open("RESULT.csv", "w"))
+for k,v in results.items():
+    w.writernow([k, v])
